39289085f3c7afd289d834c12ab9342244d88e89
limon-programming-hero <limonhasan405@gmail.com>
2023-11-25 22:09:29 +0600

    payment [done]

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ require('dotenv').config();
 var jwt = require('jsonwebtoken');
 const { MongoClient, ServerApiVersion, ObjectId, } = require('mongodb');
 const port = process.env.PORT || 3000;
+const stripe = require("stripe")(process.env.stripe_sk);
 
 // setting up middleware
 app.use(express.json())
@@ -20,7 +21,7 @@ const client = new MongoClient(uri,
             deprecationErrors: true,
         }
     })
-const errorResponse = (message) => {
+const errorResponse = (res, message) => {
     return res.status(403).send({
         error: true,
         message: message
@@ -56,17 +57,51 @@ async function run() {
         const itemsCollection = client.db('bridal-film').collection('items');
         const usersCollection = client.db('bridal-film').collection('users');
         const likesCollection = client.db('bridal-film').collection('like-items');
+        const sessionsCollection = client.db('bridal-film').collection('sessions');
         const commentsCollection = client.db('bridal-film').collection('comment-items');
+        const bookingCollection = client.db('bridal-film').collection('booking-items');
+        const paymentCollection = client.db('bridal-film').collection('payments');
 
         app.get('/', (req, res) => {
             res.send('Boss is waiting to finish')
         })
 
+        // stipe operation 
+        app.post("/create-payment-intent", jwtVerify, async (req, res) => {
+            const { price } = req.body;
+            const amount = price * 100;//making into cent from dollar
+
+            // Create a PaymentIntent with the order amount and currency
+            const paymentIntent = await stripe.paymentIntents.create({
+                amount: amount,
+                currency: "usd",
+                payment_method_types: [
+                    "card"
+                ],
+            });
+            console.log(paymentIntent)
+            res.send({
+                clientSecret: paymentIntent.client_secret,
+            });
+        });
+        // payment operations
+        app.get('/payments', jwtVerify, async (req, res) => {
+            const result = await paymentCollection.find({}).toArray();
+            res.send(result);
+        })
+        app.post('/payments', jwtVerify, async (req, res) => {
+            const { data } = req.body;
+            if (req.decoded?.email !== data?.email) {
+                return errorResponse(res, 'unauthenticated trying to add payment data, please login!')
+            }
+            const result = await paymentCollection.insertOne(data);
+            res.send(result);
+        })
 
         // items operations
         app.get('/items', async (req, res) => {
             const { email } = req.query;
-            console.log(email, req.query);
+            // console.log(email, req.query);
             if (email) {
                 const pipeLine = [
                     {
@@ -103,7 +138,7 @@ async function run() {
                         $project: { orderedItems: 0 }
                     }
                 ]
-                const allItem = await itemsCollection.aggregate(pipeLine).toArray()
+                const allItem = await itemsCollection.aggregate(pipeLine).toArray();
                 // console.log('aggregate', allItem);
                 return res.send(allItem);
             } else {
@@ -116,7 +151,7 @@ async function run() {
             const { email } = req.query;
             const { id } = req.params;
             if (email !== req?.decoded?.email) {
-                return errorResponse('unauthenticated trying to modify items, please login!')
+                return errorResponse(res, 'unauthenticated trying to modify items, please login!')
             }
             const filter = { _id: new ObjectId(id) };
             const result = await itemsCollection.deleteOne(filter);
@@ -136,7 +171,7 @@ async function run() {
             const filter = { _id: new ObjectId(id) };
             const item = await itemsCollection.findOne(filter);
             const likes = item?.likes ? item.likes : 0;
-            console.log(item?.liked + 1, likes);
+            // console.log(item?.liked + 1, likes);
             const updateDoc = {
                 $set: {
                     likes: isLike ? likes + 1 : likes - 1,
@@ -145,25 +180,94 @@ async function run() {
             const result = await itemsCollection.updateOne(filter, updateDoc);
             res.send(result);
         })
+        // booking items operations
+        // todo:make it accessible only for admin users
+        app.get('/booking', async (req, res) => {
+            const result = await bookingCollection.find({}).toArray();
+            res.send(result);
+        })
+        app.get('/booking/:email', jwtVerify, async (req, res) => {
+            const { email } = req.params;
+            if (email !== req.decoded?.email) {
+                return errorResponse(res, 'unauthorized user trying get booking data, please login!')
+            }
+            const result = await bookingCollection.find({ email: email }).toArray();
+            // console.log(result);
+            res.send(result);
+        })
+        app.post('/booking', jwtVerify, async (req, res) => {
+            const { bookingData } = req.body;
+            if (req.decoded?.email !== bookingData?.email) {
+                return errorResponse(res, 'unauthorized user trying to add booking, please login!')
+            }
+            const result = await bookingCollection.insertOne(bookingData);
+            res.send(result);
+        })
+        app.delete('/booking/:id', jwtVerify, async (req, res) => {
+            const { id } = req.params;
+            const { email } = req.query;
+            if (req.decoded?.email !== email) {
+                return errorResponse(res, 'unauthorized user trying to add booking, please login!')
+            }
+            const filter = { _id: new ObjectId(id) }
+            const result = await bookingCollection.deleteOne(filter);
+            res.send(result);
+        })
+        app.delete("/booking", jwtVerify, async (req, res) => {
+            const { email } = req.query;
+            if (req.decoded?.email !== email) {
+                return errorResponse(res, 'unauthorized user trying to delete all booking item, please login!')
+            }
+            const result = await bookingCollection.deleteMany({ email: email });
+            res.send(result);
+        })
+        // sessions operations
+        app.get('/sessions', async (req, res) => {
+            const result = await sessionsCollection.find({}).toArray();
+            res.send(result);
+        })
+        app.post('/sessions', jwtVerify, async (req, res) => {
+            const { sessionData } = req.body;
+            const { email } = req.query;
+            if (email !== req.decoded?.email) {
+                return errorResponse(res, 'unauthorized user trying add data, Please login!')
+            }
+            const result = await sessionsCollection.insertOne(sessionData);
+            res.send(result);
+        })
+        app.patch('/sessions/:id', jwtVerify, async (req, res) => {
+            const { id } = req.params;
+            const { sessionData } = req.body;
+            const { email } = req.query;
+            if (email !== req.decoded?.email) {
+                return errorResponse(res, 'unauthorized user trying add data, Please login!')
+            }
+            const filter = { _id: new ObjectId(id) };
+            const updateDoc = {
+                $set: sessionData
+            }
+            const result = await sessionsCollection.updateOne(filter, updateDoc);
+            res.send(result);
+        })
         // likes operation 
         app.post('/likes', jwtVerify, async (req, res) => {
             const verifiedEmail = req?.decoded?.email;
             const { postData } = req.body;
             const { email } = postData;
             if (verifiedEmail !== email) {
-                return errorResponse('unauthenticated trying to like, please login!');
-            }
+                return errorResponse(res, 'unauthenticated trying to like, please login!');
+            }/////taitaitai
             const result = await likesCollection.insertOne(postData);
-            console.log(result);
+            // console.log(result);
             res.send(result);
         });
         app.delete('/likes/:id', jwtVerify, async (req, res) => {
             const { id } = req.params;
             // console.log(id, req.params)
             const filter = { _id: new ObjectId(id) }
-            console.log(filter);
+            // console.log(filter);
             const result = await likesCollection.deleteOne(filter);
-            console.log({ deletedResult: result });
+            // console.log({ deletedResult: result });
             res.send(result);
         })
         // users operations 
@@ -171,18 +275,20 @@ async function run() {
             const result = await usersCollection.find({}).toArray();
             res.send(result);
         })
-        app.get('/users/:email', jwtVerify, async (req, res) => {
-            const { email } = req.params;
+        app.get('/users/isAdmin', jwtVerify, async (req, res) => {
+            const { email } = req.query;
+            // console.log(email, req.query);
             if (email !== req.decoded.email) {
-                return errorResponse('unauthenticated trying to get protected data, please login!')
+                return errorResponse(res, 'unauthenticated trying to get protected data, please login!')
             }
             const result = await usersCollection.find({ email: email }).toArray();
-            // console.log(result)
-            res.send(result);
+            const isAdmin = result[0]?.role === "admin" ? true : false;
+            // console.log({ isAdmin })
+            res.send({ isAdmin });
         })
         app.post('/users', async (req, res) => {
             const { userDetails, email } = req.body;
-            console.log(userDetails, email)
+            // console.log(userDetails, email)
             const result = await usersCollection.insertOne(userDetails);
             res.send(result);
         })
@@ -191,7 +297,7 @@ async function run() {
             const { updateInfo } = req.body;
 
             if (req?.decoded?.email !== email) {
-                return errorResponse('unauthenticated trying to update data, please login!')
+                return errorResponse(res, 'unauthenticated trying to update data, please login!')
             }
             const filter = { _id: new ObjectId(updateInfo?.id) };
             const updateDoc = {
@@ -205,7 +311,7 @@ async function run() {
             const { email } = req.query;
 
             if (req?.decoded?.email !== email) {
-                return errorResponse('unauthenticated trying to delete data, please login!')
+                return errorResponse(res, 'unauthenticated trying to delete data, please login!')
             }
             const filter = { _id: new ObjectId(id) };
             const result = await usersCollection.deleteOne(filter);
@@ -220,7 +326,8 @@ async function run() {
         // jwt token sign in
         app.post('/jwt-signIn', async (req, res) => {
             const email = req.body;
-            const token = await jwt.sign(email, `${process.env.jwt_token}`, { expiresIn: '2d' });
+            // console.log(email, req.body);
+            const token = jwt.sign(email, `${process.env.jwt_token}`, { expiresIn: '2d' });
             // console.log(token);
             res.send(token);
         })
